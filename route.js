7d8af5321099d0b4fc0fa9ae17671108135627e3
MatinSoleymani <matinsoleymni@gmail.com>
2023-05-18 22:27:35 +0330

    update route

--- route.js
+++ route.js
@@ -1,11 +1,11 @@
 const $ = document;
-const domin = 'http://127.0.0.1:5500/';
+const domin = 'http://127.0.0.1:5500/SingleWebApp/';
 const info  = $.querySelector('.info');
 const title = $.querySelector('.title');
 const diti  = $.querySelector('.dit');
-console.log($.documentURI)
+console.log($.URL)
 
-if($.documentURI == domin+'index.html'){
+if($.URL == domin+'index.html' || $.URL == domin){
     title.innerHTML = 'index.html'
     diti.innerHTML  = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, vero!'; 
 }else if($.documentURI == domin+'index.html?php=admin'){
