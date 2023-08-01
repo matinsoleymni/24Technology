997bfff59ce4386eeaebf98384ae53b7f72868df
Matin Soleymani <matinsoleymni@gmail.com>
2023-08-01 17:14:00 +0330

    V.2 Started :)

--- webserver.js
+++ webserver.js
@@ -1,39 +1,38 @@
-/* WEBSERVER  */
-if($.URL == domin|| $.URL == domin+'#' || $.URL == domin+'index.html'){
-    fetch("./assets/layouts/HomePage.html")
-    .then(res => res.text())
-    .then(txt => APP.innerHTML = txt);
+const $ = document;
+const ROOT = $.querySelector('.ROOT')
+const APP = $.querySelector('.APP')
 
-}else if ($.URL == domin+'#Blog' || $.URl == domin+'index.html#Blog'){
-    fetch("./assets/layouts/Blog.html")
-    .then(res => res.text())
-    .then(txt => APP.innerHTML = txt);
-}else if($.URL == domin+'#login' || $.URL == domin+'index.html#login'){
-    fetch('./assets/layouts/login.html')
-    .then(res => res.text())
-    .then(txt => APP.innerHTML= txt)
-}else {
-    APP.innerHTML = '403'
+const Router = {
+    "/Blog": {
+        template: "Blog.html",
+        title: "Hello",
+        metaTags: "",
+        APP:true
+    }
 }
 
-window.addEventListener('locationchange' , ()=> {
-    if($.URL == domin || $.URL == domin+'#' || $.URL == domin+'index.html'){
-        fetch("./assets/layouts/HomePage.html")
-        .then(res => res.text())
-        .then(txt => APP.innerHTML = txt);
-    }else if ($.URL == domin+'#Blog' || $.URl == domin+'index.html#Blog'){
-        fetch("./assets/layouts/Blog.html")
-        .then(res => res.text())
-        .then(txt => APP.innerHTML = txt);
-    }else if($.URL == domin+'#login' || $.URL == domin+'index.html#login'){
-        fetch('./assets/layouts/login.html')
-        .then(res => res.text())
-        .then(txt => APP.innerHTML= txt)
-    }else if($.URL == domin+'#Products' || $.URL == domin+'index.html$Products'){
-        fetch('./assets/layouts/Products.html')
-        .then(res => res.text())
-        .then(txt => APP.innerHTML= txt)
-    }else {
-        APP.innerHTML = '404'
-    }
+document.addEventListener('click' , (e)=> {
+    if(!e.target.className.includes("route")){return false}
+    e.preventDefault();
+    window.history.pushState({} , "" , e.target.href);
+    handler()
+
 })
+
+document.addEventListener('load' , handler())
+
+async function handler(){
+    const route = Router[window.location.pathname];
+    if(!route){
+        generate(404 , {APP:true});
+    }
+    await fetch(route.template).then((res) => res.text()).then(data => generate(data , route))
+}
+
+function generate(d , r){
+    if(r.APP){
+       APP.innerHTML = d
+    }else if(r.ROOT){
+        ROOT.innerHTML = d
+    }
+}
